6017fa2a85f6c82b121509c13c961c1384ae14e6
Eduardo Tongson <propolice@gmail.com>
2021-04-15 19:40:20 +0800

    ENTRYPOINT takes a table.

--- buildah.lua
+++ buildah.lua
@@ -249,9 +249,10 @@ local from = function(base, assets, cid)
   --++ Sets the container entrypoint.
   --++ NOTE: Only accepts a single entrypoint item, usually the executable.
   --++
-  env.CONFIG = function(entrypoint, cmd, term)
-    cmd = cmd or [['']]
-    term = term or 'TERM'
+  env.CONFIG = function(config)
+    local entrypoint = config.entrypoint
+    local cmd = config.cmd or [['']]
+    local term = config.term or 'TERM'
     local r, so, se = buildah{
       'config';
       '--entrypoint';
