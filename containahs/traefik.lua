262b2edbf1369b0f8a51cc4a5d2d3ccd0d861b47
Eduardo Tongson <propolice@gmail.com>
2021-04-29 21:18:35 +0800

    Traefik does not support JSON configs.

--- containahs/traefik.lua
+++ containahs/traefik.lua
@@ -1,5 +1,5 @@
 require("buildah")
 FROM("gcr.io/distroless/static", "traefik")
 UPLOAD("traefik.v2.4.8", "/traefik")
-ENTRYPOINT("/traefik", "--config", "/config/traefik.json")
+ENTRYPOINT("/traefik", "--config", "/config/traefik.yaml")
 COMMIT("traefik:2.4.8")
