41fb1e917352e520b702e082dcec9b7ca5671a8a
Eduardo Tongson <propolice@gmail.com>
2021-04-30 20:07:47 +0800

    Initial commit for kapow.

---
+++ containahs/kapow.lua
@@ -0,0 +1,6 @@
+require("buildah")
+FROM("gcr.io/distroless/static", "kapow")
+UPLOAD("kapow.v0.7.0", "/kapow")
+UPLOAD("/usr/bin/ll", "/usr/bin/ll")
+ENTRYPOINT("/kapow", "server")
+COMMIT("kapow:0.7.0")
