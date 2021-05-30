00db7151a508419545f377523738556d188685c2
Eduardo Tongson <propolice@gmail.com>
2021-05-30 19:45:28 +0800

    Required for some things.

--- containahs/tumbleweed.lua
+++ containahs/tumbleweed.lua
@@ -1,7 +1,7 @@
 FROM("docker://docker.io/opensuse/tumbleweed:latest", "devel")
 RUN("zypper dup -y")
 RUN("zypper install -y -t pattern devel_basis")
-RUN("zypper install -y --no-recommends pcre-devel libopenssl-devel curl git go1.16 neovim")
+RUN("zypper install -y --no-recommends pcre-devel libopenssl-devel curl git go1.16 neovim lz4")
 SH("echo -n 'nameserver 127.255.255.53' > /etc/resolv.conf")
 TAR("devel.tar")
 COMMIT("devel:latest")
